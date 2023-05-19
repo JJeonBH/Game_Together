fd9882901a4d7e093222bb2bc944615ce6532286
JJeonBH <wwjjss5386@gmail.com>
2023-05-19 14:17:05 +0900

    개인 프로젝트 기본 설정

---
+++ src/main/java/com/koreaIT/Game_Together/GameTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.koreaIT.Game_Together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koreaIT/Game_Together/GameTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreaIT.Game_Together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
